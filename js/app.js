ceefccf82e4d276cdab52208707c1874010284c9
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-11-02 17:27:23 +0000

    dashboard update

--- js/app.js
+++ js/app.js
@@ -5,4 +5,10 @@ window.addEventListener("load", () => {
         location: "",
         date: ""
     }
+
+    const startUpTable = document.querySelector("#companies");
+    const companyInput = document.querySelector("#companyName");
+    const marketInput = document.querySelector("#marketName");
+    const locationInput = document.querySelector("#locationName");
+    const dateInput = document.querySelector("#specificDate");
 })
